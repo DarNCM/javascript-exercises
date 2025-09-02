4bfe16798ceb1f0f1523a2d34d541934f9976bc6
DarNCM <216679882+DarNCM@users.noreply.github.com>
2025-09-02 15:03:48 +0200

    done

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const { workerData } = require("worker_threads");
 
-};
+const repeatString = function(word, number) { //Erklärung: Diese Funktion wiederholt einen bestimmten String eine bestimmte Anzahl von Malen.
+  if(number < 0) 
+    return("ERROR"); //Erklärung: Wenn die Anzahl kleiner als 0 ist, wird eine Fehlermeldung ausgegeben.
+     let string =""; //Erklärung: Diese Variable speichert den wiederholten String.
+       for (let i = 0; i < number; i++ ){ //Erklärung: Diese Schleife wiederholt den String so oft, wie die Anzahl angibt.
+         string += word //Erklärung: Diese Zeile fügt den String zur Variablen string hinzu.
+         
+}
+return (string);
+};  
+  
+
+console.log(repeatString("he o ", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
+
