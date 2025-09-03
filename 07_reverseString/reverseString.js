401aadb8b43a4a4fcb1b52a0df4ce20e0c73f2ba
DarNCM <216679882+DarNCM@users.noreply.github.com>
2025-09-03 09:18:21 +0200

    Done

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return string.split("").reverse().join(""); //Erklärung: Diese Funktion gibt den String in umgekehrter Reihenfolge zurück. 
+    //.split() = Diese Methode teilt einen String in ein Array von Teilstrings auf.
+    //.reverse() = Diese Methode kehrt die Reihenfolge der Elemente in einem Array um.
+    //.join() = Diese Methode verbindet alle Elemente eines Arrays zu einem String.
 };
 
 // Do not edit below this line
